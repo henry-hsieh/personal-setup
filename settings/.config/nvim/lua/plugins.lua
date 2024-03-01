c92ab7c987c7e9b99fb6349e23d18f0121ed281a
Henry Hsieh <r901042004@yahoo.com.tw>
2024-03-02 00:36:52 +0800

    Revert the mason registry

--- settings/.config/nvim/lua/plugins.lua
+++ settings/.config/nvim/lua/plugins.lua
@@ -293,9 +293,9 @@ require("lazy").setup({
     config = function()
       require'mason'.setup{
         -- log_level = vim.log.levels.DEBUG,
-        --registries = {
-        --    "github:henry-hsieh/mason-registry",
-        --},
+        registries = {
+            "github:henry-hsieh/mason-registry",
+        },
       }
     end
   },
