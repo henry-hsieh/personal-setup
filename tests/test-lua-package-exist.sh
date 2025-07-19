883f0394a31db5def406f490654eacf5ffb687b3
Henry Hsieh <r901042004@yahoo.com.tw>
2025-07-19 14:37:02 +0800

    test(nvim-plugins): resolve test errors due to lazy load

--- tests/test-lua-package-exist.sh
+++ tests/test-lua-package-exist.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ "$#" == 2 ]]; then
-  nvim --headless -c "lua require(\"$1\"); vim.api.nvim_command(\"qall\")" +cq $2 && echo "Test $1 pass"
-elif [[ "$#" == 1 ]]; then
-  nvim --headless -c "lua require(\"$1\"); vim.api.nvim_command(\"qall\")" +cq && echo "Test $1 pass"
+if [[ "$#" == 3 ]]; then
+  nvim --headless -c "set ft=$3" -c "Lazy! load $1" -c "lua local ok, _ = pcall(require, \"$2\"); if ok then print(\"Test $1 pass\n\") else print(\"Test $1 fail\n\") vim.cmd('cq 1') end" -c "q"
+elif [[ "$#" == 2 ]]; then
+  nvim --headless -c "Lazy! load $1" -c "lua local ok, _ = pcall(require, \"$2\"); if ok then print(\"Test $1 pass\n\") else print(\"Test $1 fail\n\") vim.cmd('cq 1') end" -c "q"
 else
-  echo "usage: $0 lua_package_name [filename_to_trigger_lazy_ft]" > /dev/stderr
+  echo "usage: $0 lua_plugin_name lua_module_name [filetype]" > /dev/stderr
+  echo "lua_plugin_name: The repository name of the plugin"
+  echo "lua_module_name: The main lua filename of the plugin"
+  echo "filetype: If the plugin is lazy-loaded by filetype, the filetype should be specified"
   exit 1
 fi
