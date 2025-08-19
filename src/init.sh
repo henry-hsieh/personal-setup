0c49c7f6ec37bd7c34d2a15d2853e2e2b67e98c4
Henry Hsieh <r901042004@yahoo.com.tw>
2025-08-19 21:37:12 +0800

    fix(nvim): download blink.cmp fuzzy prebuilt binaries (#309)

--- src/init.sh
+++ src/init.sh
@@ -28,3 +28,17 @@ nvim --headless -c "MasonToolsUpdateSync" +qa
 # Ensure nvim-treesitter parsers are installed
 print_process_item "Install/update Neovim tree-sitter parsers"
 (export CFLAGS="$CFLAGS -std=c11"; cd $ROOT_DIR/src; nvim --headless -c 'lua dofile("install-ts-parser.lua")' +qa!)
+
+# Download blink.cmp fuzzy prebuilt binaries
+print_process_item "Install Neovim blink.cmp prebuilt binaries"
+blink_version=$(git -C $HOME/.local/share/nvim/lazy/blink.cmp describe --exact-match --tags)
+if [[ "$blink_version" == "" ]]; then
+  echo "blink.cmp is not on the release version"
+  exit 1
+fi
+mkdir -p $HOME/.local/share/nvim/lazy/blink.cmp/target/release/
+pushd $HOME/.local/share/nvim/lazy/blink.cmp/target/release/ > /dev/null
+download_exe  https://github.com/Saghen/blink.cmp/releases/download/$blink_version/x86_64-unknown-linux-gnu.so libblink_cmp_fuzzy.so
+download_file https://github.com/Saghen/blink.cmp/releases/download/$blink_version/x86_64-unknown-linux-gnu.so.sha256 libblink_cmp_fuzzy.so.sha256
+echo $blink_version > version
+popd > /dev/null
