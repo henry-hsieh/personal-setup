b3634936f59f46acfdd74e5804cae2d6233a40d2
Henry Hsieh <r901042004@yahoo.com.tw>
2025-08-30 01:38:30 +0800

    feat(nvim): add trouble.nvim (#324)

--- src/settings/.config/nvim/lua/plugins.lua
+++ src/settings/.config/nvim/lua/plugins.lua
@@ -485,6 +485,7 @@ require("lazy").setup({
         snacks_notif = "Notification",
         snacks_notif_history = "Notification History",
         snacks_terminal = "Terminal",
+        trouble = "Trouble",
       }
 
       -- Helpers
@@ -664,6 +665,24 @@ require("lazy").setup({
         winbar = make_winbar(true),
         inactive_winbar = make_winbar(false),
       }
+
+      -- trouble.nvim
+      local trouble = require("trouble")
+      local symbols = trouble.statusline({
+        mode = "lsp_document_symbols",
+        groups = {},
+        title = false,
+        filter = { range = true },
+        format = "{kind_icon}{symbol.name:Normal}",
+        -- The following line is needed to fix the background color
+        hl_group = "lualine_c_normal",
+      })
+      vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
+      table.insert(opts.sections.lualine_c, {
+        symbols.get,
+        cond = symbols.has,
+      })
+
       return opts
     end
   },
@@ -1147,6 +1166,44 @@ require("lazy").setup({
       })
       vim.cmd.doautocmd("User TintedColorsPost")
     end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
 },
 {
