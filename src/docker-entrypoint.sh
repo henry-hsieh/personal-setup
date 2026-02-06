414ade2bd7afe2fceb3fde8d150c956e9d12748e
Henry Hsieh <r901042004@yahoo.com.tw>
2026-02-06 22:46:47 +0800

    build!: migrate to makeself to create all-in-one release

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Read UID and GID from env (or default to 1000)
 USER_ID=${TARGET_UID:-1000}
 GROUP_ID=${TARGET_GID:-1000}
@@ -18,9 +18,6 @@ fi
 # Change owner of user's home directory
 chown $USER_ID:$GROUP_ID $USER_HOME
 
-# Give passwordless sudo
-echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/$USER_NAME
-chmod 0440 /etc/sudoers.d/$USER_NAME
-
 # Switch to the user
-exec sudo -E -u "$USER_NAME" env PATH=$PATH HOME=$USER_HOME "$@"
+args=$(printf ' %q' "$@")
+su -c "export PATH=$PATH; $args" $USER_NAME
